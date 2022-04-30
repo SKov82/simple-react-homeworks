3d1f150e80f79fea3c075d7d51480f1a8d3a9a5a
skov82 <skov82@ya.ru>
2022-04-30 16:59:03 +0300

    Updates

---
+++ precache-manifest.8eecbe80dbbee094b734a928979b3d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b307af21778ac0220c39a681d4c87f4b",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "3408eb77ab215cffc8ba",
+    "url": "/simple-react-homeworks/static/css/main.47db350a.chunk.css"
+  },
+  {
+    "revision": "f6080bd0279fbeec6242",
+    "url": "/simple-react-homeworks/static/js/2.8e8954fd.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/simple-react-homeworks/static/js/2.8e8954fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3408eb77ab215cffc8ba",
+    "url": "/simple-react-homeworks/static/js/main.958eb349.chunk.js"
+  },
+  {
+    "revision": "0b1e09e39c3a6595d4d4",
+    "url": "/simple-react-homeworks/static/js/runtime-main.8d215da6.js"
+  }
+]);
